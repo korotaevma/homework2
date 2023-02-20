00201944c0784cd9d7e19c0a7012fa5142d00a25
Maksim <korotaevma@gmail.com>
2023-02-20 19:28:32 +0500

    Решение15

---
+++ task15/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите день недели: ");
+int day = Convert.ToInt32(Console.ReadLine());
+string dayText = Convert.ToString(day);
+if (day < 6)
+{
+    Console.WriteLine("Рабочий день!");
+}
+else
+  {
+     Console.WriteLine("Выходной день!");
+  }
